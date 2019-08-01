b1369b49b651846cd46d06a2abb717feb01dab80
Ishtiaq Mahmood Amin <i4idrhine@gmail.com>
2019-08-02 01:17:55 +0600

    final and const diffs and defs

--- 4constants.dart
+++ 4constants.dart
@@ -10,6 +10,12 @@ void main() {
   const double gravity = 9.8;
 }
 
+/*Difference between final and const are:
+‣final variable can only be set once and it is initialized when accessed.
+‣const variable is implicitly final but it is a compile-time constant. memory allocated while compiling. 
+
+Class instance variable can be final but cant be const.
+*/
 class Circle {
   final color = 'Red';
   static const PI = 3.14;
