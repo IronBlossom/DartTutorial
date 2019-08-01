075982f5c11da5aedf484e29bf234ef3fe1a7e4b
Ishtiaq Mahmood Amin <i4idrhine@gmail.com>
2019-08-02 00:39:51 +0600

    Data types finding.

--- 2built_in_data_types.dart
+++ 2built_in_data_types.dart
@@ -3,6 +3,8 @@ void main(List<String> arguments) {
   int score = 23;
   var count = 23; // It is inferred as integer automatically by compiler
   int hexValue = 0xEADEBAEE;
+  /* This is same as java but unlike Java, which has 2 bytes of int max,
+  dart has no size limit on int if it compiles for Dart VM */
 
   // Numbers: double
   double percentage = 93.4;
@@ -22,4 +24,7 @@ void main(List<String> arguments) {
 
   // NOTE: All data types in Dart are Objects.
   // Therefore, their initial value is by default 'null'
+
+  /* Everything is object means boolean value can be null as well, There are no primitives. */
+/*There are other types of data on dart like Runes, map etc. which will be discussed later. */
 }
