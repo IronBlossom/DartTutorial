7af7b1454777a84fe94658f125dd0eb0f5f8ade9
Ishtiaq Mahmood Amin <imamin@ael-bd.com>
2019-11-21 19:08:38 +0600

    Checking out while backing up imamin macmini to github repo , on github-imamin-Devel branch

--- 8_for_loop.dart
+++ 8_for_loop.dart
@@ -1,18 +1,12 @@
+import 'dart:core';
+
 void main() {
-  // FOR Loop
+  int i = 2;
+  var temp = ++i;
+  print(temp);
 
-  // WAP to find the even numbers between 1 to 10
+  List growablelist = ["Hello"];
 
-  for (int i = 1; i <= 10; i++) {
-    if (i % 2 == 0) {
-      print(i);
-    }
-  }
 
-  // for ..in loop
-  List planetList = ["Mercury", "Venus", "Earth", "Mars"];
-
-  for (String planet in planetList) {
-    print(planet);
-  }
+  print(growablelist);
 }
